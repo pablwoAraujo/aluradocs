bab51fd30944683c006318fdd05f2820c2a0ea74
Pablwo Araujo <pablwo.araujo@ccc.ufcg.edu.br>
2023-10-26 17:13:33 -0300

    feat: adicionando o socket.io no projeto

---
+++ src/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import url from "url";
+import path from "path";
+import http from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const currentPath = url.fileURLToPath(import.meta.url);
+const publicDirectory = path.join(currentPath, "../..", "public");
+
+app.use(express.static(publicDirectory));
+
+const httpServer = http.createServer(app);
+
+httpServer.listen(PORT, () => console.log("listening on port: " + PORT));
+
+const io = new Server(httpServer);
+
+io.on("connection", () => {
+  console.log("Um cliente se conectou!");
+})
